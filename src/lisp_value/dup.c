b2580cb1c42df15baede3e51212af106d424fd2f
Curtis McEnroe <programble@gmail.com>
2012-02-22 22:33:58 -0500

    Regulate lisp type ordering in dup.c

--- src/lisp_value/dup.c
+++ src/lisp_value/dup.c
@@ -6,17 +6,17 @@
 lisp_value *lisp_value_dup(lisp_value *this)
 {
     switch (this->type) {
+    case LISP_TYPE_INT: {
+        int *i = talloc(NULL, int);
+        *i = *LISP_INT(this);
+        return lisp_value_new_int(i);
+    case LISP_TYPE_SYMBOL:
+        return lisp_value_new_symbol(talloc_strdup(NULL, LISP_SYMBOL(this)));
     case LISP_TYPE_CONS:
         if (LISP_CONS_NIL(this))
             return lisp_value_new_cons_nil();
         else
             return lisp_value_new_cons(lisp_value_dup(LISP_CONS_CAR(this)), lisp_value_dup(LISP_CONS_CDR(this)));
-    case LISP_TYPE_SYMBOL:
-        return lisp_value_new_symbol(talloc_strdup(NULL, LISP_SYMBOL(this)));
-    case LISP_TYPE_INT: {
-        int *i = talloc(NULL, int);
-        *i = *LISP_INT(this);
-        return lisp_value_new_int(i);
     default:
         assert(0); return NULL;
     }
